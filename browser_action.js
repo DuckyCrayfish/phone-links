411885f9dd0215157f9276256d7442b29b5e8a1a
Nick Iacullo <nickiacullo@gmail.com>
2019-07-31 10:27:51 -0500

    Store reused variables for faster lookups

--- browser_action.js
+++ browser_action.js
@@ -16,11 +16,14 @@ const linkTextFormat = document.getElementById('linkTextFormat');
 
 const saveButton = document.getElementById('saveButton');
 
+let currentTab;
+
 
 chrome.tabs.query({ active: true, currentWindow: true }, ([activeTab]) => initializeDOM(activeTab));
 chrome.tabs.onActivated.addListener(activeInfo => chrome.tabs.get(activeInfo.tabId, initializeDOM));
 
 function initializeDOM(activeTab) {
+    currentTab = activeTab;
     const domain = encodeURI(activeTab.url.match(regexDomain)[1]);
     const url = encodeURI(activeTab.url);
 
@@ -34,110 +37,105 @@ function initializeDOM(activeTab) {
         customTel: [],
         customText: []
     }, function(settings) {
-        filterDomainCheckbox.checked = settings.ignoredDomains.indexOf(domain) > -1;
         filterURLCheckbox.checked = settings.ignoredURLS.indexOf(url) > -1;
-        customFormatCheckbox.checked = settings.useCustom.indexOf(domain) > -1;
-        if (settings.useCustom.indexOf(domain) > -1) {
-            let domainIndex = settings.useCustom.indexOf(domain);
+        filterDomainCheckbox.checked = settings.ignoredDomains.indexOf(domain) > -1;
+        let customIndex = settings.useCustom.indexOf(domain);
+        customFormatCheckbox.checked = customIndex > -1;
+        if (customIndex > -1) {
             customFormatRow.style.display = "block";
-            telLinkFormat.value = settings.customTel[domainIndex];
-            linkTextFormat.value = settings.customText[domainIndex];
+            telLinkFormat.value = settings.customTel[customIndex];
+            linkTextFormat.value = settings.customText[customIndex];
+        } else {
+            customFormatRow.style.display = "none";
         }
     });
 }
 
 saveButton.addEventListener("click", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(arrayOfTabs) {
-        const activeTab = arrayOfTabs[0];
-        const domain = encodeURI(activeTab.url.match(regexDomain)[1]);
-        chrome.storage.local.get({
-            useCustom: [],
-            customTel: [],
-            customText: []
-        }, function(settings) {
-            if (settings.useCustom.indexOf(domain) > -1) {
-                let domainIndex = settings.useCustom.indexOf(domain);
-                settings.customTel[domainIndex] = telLinkFormat.value;
-                settings.customText[domainIndex] = linkTextFormat.value;
-            }
-            chrome.storage.local.set({
-                customTel: settings.customTel,
-                customText: settings.customText
-            }, chrome.tabs.reload);
-        });
+    const activeTab = currentTab;
+    const domain = encodeURI(activeTab.url.match(regexDomain)[1]);
+    chrome.storage.local.get({
+        useCustom: [],
+        customTel: [],
+        customText: []
+    }, function(settings) {
+        let customIndex = settings.useCustom.indexOf(domain);
+        if (customIndex > -1) {
+            settings.customTel[customIndex] = telLinkFormat.value;
+            settings.customText[customIndex] = linkTextFormat.value;
+        }
+        chrome.storage.local.set({
+            customTel: settings.customTel,
+            customText: settings.customText
+        }, chrome.tabs.reload);
     });
 });
 
 customFormatCheckbox.addEventListener("change", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(arrayOfTabs) {
-        const activeTab = arrayOfTabs[0];
-        const domain = encodeURI(activeTab.url.match(regexDomain)[1]);
-        chrome.storage.local.get({
-            useCustom: [],
-            customTel: [],
-            customText: [],
-            telLinkFormat: defaultTelFormat,
-            linkTextFormat: defaultTextFormat
-        }, function(settings) {
-            let checked = customFormatCheckbox.checked;
-            if (checked && settings.useCustom.indexOf(domain) < 0)
-                settings.useCustom.push(domain);
-            else if (!checked && settings.useCustom.indexOf(domain) > -1) {
-                settings.customTel.splice(settings.useCustom.indexOf(domain), 1);
-                settings.customText.splice(settings.useCustom.indexOf(domain), 1);
-                settings.useCustom.splice(settings.useCustom.indexOf(domain), 1);
-            }
-            customFormatRow.style.display = settings.useCustom.indexOf(domain) > -1 ? "block" : "none";
-            if (settings.useCustom.indexOf(domain) > -1) {
-                let domainIndex = settings.useCustom.indexOf(domain);
-                settings.customTel.splice(domainIndex, 0, settings.telLinkFormat);
-                settings.customText.splice(domainIndex, 0, settings.linkTextFormat);
-                telLinkFormat.value = settings.customTel[domainIndex];
-                linkTextFormat.value = settings.customText[domainIndex];
-            }
-            chrome.storage.local.set({
-                useCustom: settings.useCustom,
-                customTel: settings.customTel,
-                customText: settings.customText
-            }, chrome.tabs.reload);
-        });
+    const activeTab = currentTab;
+    const domain = encodeURI(activeTab.url.match(regexDomain)[1]);
+    chrome.storage.local.get({
+        useCustom: [],
+        customTel: [],
+        customText: [],
+        telLinkFormat: defaultTelFormat,
+        linkTextFormat: defaultTextFormat
+    }, function(settings) {
+        let checked = customFormatCheckbox.checked;
+        let customIndex = settings.useCustom.indexOf(domain);
+        if (checked && customIndex < 0) {
+            customIndex = settings.useCustom.push(domain) - 1;
+            customFormatRow.style.display = "block";
+            settings.customTel.splice(customIndex, 0, settings.telLinkFormat);
+            settings.customText.splice(customIndex, 0, settings.linkTextFormat);
+            telLinkFormat.value = settings.customTel[customIndex];
+            linkTextFormat.value = settings.customText[customIndex];
+        } else if (!checked && customIndex > -1) {
+            settings.customTel.splice(customIndex, 1);
+            settings.customText.splice(customIndex, 1);
+            settings.useCustom.splice(customIndex, 1);
+            customFormatRow.style.display = "none";
+        }
+        chrome.storage.local.set({
+            useCustom: settings.useCustom,
+            customTel: settings.customTel,
+            customText: settings.customText
+        }, chrome.tabs.reload);
     });
 });
 
 filterDomainCheckbox.addEventListener("change", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(arrayOfTabs) {
-        const activeTab = arrayOfTabs[0];
-        const domain = encodeURI(activeTab.url.match(regexDomain)[1]);
-        chrome.storage.local.get({
-            ignoredDomains: []
-        }, function(settings) {
-            let checked = filterDomainCheckbox.checked;
-            if (checked && settings.ignoredDomains.indexOf(domain) < 0)
-                settings.ignoredDomains.push(domain);
-            else if (!checked && settings.ignoredDomains.indexOf(domain) > -1)
-                settings.ignoredDomains.splice(settings.ignoredDomains.indexOf(domain), 1);
-            chrome.storage.local.set({
-                ignoredDomains: settings.ignoredDomains
-            }, chrome.tabs.reload);
-        });
+    const activeTab = currentTab;
+    const domain = encodeURI(activeTab.url.match(regexDomain)[1]);
+    chrome.storage.local.get({
+        ignoredDomains: []
+    }, function(settings) {
+        let checked = filterDomainCheckbox.checked;
+        let domainIndex = settings.ignoredDomains.indexOf(domain);
+        if (checked && domainIndex < 0)
+            settings.ignoredDomains.push(domain);
+        else if (!checked && domainIndex > -1)
+            settings.ignoredDomains.splice(domainIndex, 1);
+        chrome.storage.local.set({
+            ignoredDomains: settings.ignoredDomains
+        }, chrome.tabs.reload);
     });
 });
 
 filterURLCheckbox.addEventListener("change", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(arrayOfTabs) {
-        const activeTab = arrayOfTabs[0];
-        const url = encodeURI(activeTab.url);
-        chrome.storage.local.get({
-            ignoredURLS: []
-        }, function(settings) {
-            let checked = filterURLCheckbox.checked;
-            if (checked && settings.ignoredURLS.indexOf(url) < 0)
-                settings.ignoredURLS.push(url);
-            else if (!checked && settings.ignoredURLS.indexOf(url) > -1)
-                settings.ignoredURLS.splice(settings.ignoredURLS.indexOf(url), 1);
-            chrome.storage.local.set({
-                ignoredURLS: settings.ignoredURLS
-            }, chrome.tabs.reload);
-        });
+    const activeTab = currentTab;
+    const url = encodeURI(activeTab.url);
+    chrome.storage.local.get({
+        ignoredURLS: []
+    }, function(settings) {
+        let checked = filterURLCheckbox.checked;
+        let urlIndex = settings.ignoredURLS.indexOf(url);
+        if (checked && urlIndex < 0)
+            settings.ignoredURLS.push(url);
+        else if (!checked && urlIndex > -1)
+            settings.ignoredURLS.splice(urlIndex, 1);
+        chrome.storage.local.set({
+            ignoredURLS: settings.ignoredURLS
+        }, chrome.tabs.reload);
     });
 });
